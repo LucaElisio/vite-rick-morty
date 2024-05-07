5cb8d825e4539f9dae08822a0eb984ddab0d7dc8
LucaElisio <personal.elix@gmail.com>
2024-05-07 15:59:53 +0200

    feat: searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script>
 import axios from "axios"
 import CardList from './components/CardList.vue';
+import AppSearch from './components/AppSearch.vue';
+import { store } from "./store";
 
 export default {
   components: { 
-    CardList 
+    CardList,
+    AppSearch,
   },
   data() {
     return {
       characterArray: [],
+      store,
     };
   },
   created() {
@@ -19,11 +23,39 @@ export default {
       this.characterArray = resp.data.results;
     });
   },
+  methods: {
+    showChar(){
+      // console.log("ciao");
+      console.log(this.store.selectedStatus);
+
+      // Setto i parametri per la chiamata tramite axios
+      const paramsObj = {
+        status: ""
+      }
+
+      // Applico il filtro
+      if (this.store.selectedStatus !== "All") {
+        paramsObj.status = this.store.selectedStatus;
+      }
+
+      // Eseguo chiamata axios
+      axios.
+      get("https://rickandmortyapi.com/api/character", {
+        params: paramsObj
+      })
+      .then((resp) => {
+      console.log(resp)
+      this.characterArray = resp.data.results;
+    });
+
+    }
+  }
 };
 </script>
 
 <template>
   <h1>Rick and Morty App</h1>
+  <AppSearch @statusChanged="showChar"/>
   <CardList :characterArray="characterArray"/>
   <!-- <ul>
     <li v-for="curChar in characterArray">{{curChar.name}}</li>
